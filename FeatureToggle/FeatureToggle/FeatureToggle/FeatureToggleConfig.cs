a5fafc891a7fb41339c5f287fe1edfc8df9936bc
Evrim <evrim@tr.ibm.com>
2018-11-01 00:26:47 +0300

    initial commit

---
+++ FeatureToggle/FeatureToggle/FeatureToggle/FeatureToggleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleMVC.FeatureToggle
+{
+    public class FeatureToggleConfig : Dictionary<string, ControllerToggle>
+    {
+    }
+
+    public class ControllerToggle
+    {
+        public bool Enabled { get; set; }
+        public List<string> EnabledMethods { get; set; }
+        public List<string> DisabledMethods { get; set; }
+    }
+}
